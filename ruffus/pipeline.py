e0df9403b2f1da511a88780d1da402a09dadeb04
denis-torre <denis.torre@mssm.edu>
2018-12-10 14:35:06 -0500

    Ruffus

---
+++ ruffus/pipeline.py
@@ -0,0 +1,102 @@
+
+# Import ruffus
+from ruffus import *
+from ruffus.combinatorics import *
+
+# Function to create files
+import pathlib, time
+def createFile(input, output):
+    time.sleep(0.1)
+    pathlib.Path(output).touch()
+    print('\nFinished job:\nINPUT: {input}\nOUTPUT: {output}\n'.format(**locals()))
+
+#############
+### Files
+#############
+fastq_path = '../data/fastq/*.fastq'
+genome_path = '../data/genome.fa'
+
+########################################
+##### 1. Align Reads
+########################################
+# Create directory
+@follows(mkdir('1-counts'))
+
+# Transform - 1 to 1 operation with regex pattern matching, plus additional input
+@transform(fastq_path,
+           regex(r'.*/(.*).fastq'),
+           add_inputs(genome_path),
+           r'1-counts/\1-counts.txt')
+
+def getCounts(infile, outfile):
+
+    createFile(infile, outfile)
+
+########################################
+##### 2. Merge counts
+########################################
+# Create directory
+@follows(mkdir('2-merged'))
+
+# Merge - many to 1 operation
+@merge(getCounts,
+       '2-merged/counts.txt')
+
+def mergeCounts(infile, outfile):
+
+    createFile(infile, outfile)
+
+########################################
+##### 3. Split Dataset
+########################################
+# Create directory
+@follows(mkdir('3-groups'))
+
+# Subdivide - one to many operation
+@subdivide(mergeCounts,
+           formatter(),
+           '3-groups/group*.txt',
+           '3-groups/group')
+
+def splitData(infile, outfiles, outfileRoot):
+
+    for i in range(1, 6):
+        outfile = '{outfileRoot}{i}.txt'.format(**locals())
+        createFile(infile, outfile)
+
+########################################
+##### 4. Differential Expression
+########################################
+# Create directory
+@follows(mkdir('4-differential_expression'))
+
+# Combinations - many to many operation
+@combinations(splitData,
+              formatter(),
+              2,
+              '4-differential_expression/{basename[0][0]}_vs_{basename[1][0]}.txt')
+
+def runDifferentialExpression(infile, outfile):
+
+    createFile(infile, outfile)
+
+pipeline_run()
+
+########################################
+##### 5. Merge
+########################################
+# Create directory
+@follows(mkdir('5-results'))
+
+# Merge - many to pne operation
+@merge(runDifferentialExpression,
+      '5-results/merged_results.txt')
+
+def mergeResults(infile, outfile):
+
+    createFile(infile, outfile)
+
+#############
+### Run
+#############
+pipeline_run(getCounts)
